210000928f5caba3aac1cc2eb689fe4b0ef7739d
AHOUSAIN <afrahhassancmu@gmail.com>
2012-04-20 17:40:20 +0300

    Sign in page is working

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -12,5 +12,47 @@ render_views
         response.should have_selector("title", :content => "Sign in")
       end
   end
+  
+  describe "Post 'create'" do
+    describe "failure" do
+      before (:each) do
+        @attr = {:email => "email@example.com" ,:password => "invalid"}
+      end
+      
+      it " should have the right title" do
+        post :create , :session => @attr
+       response.should have_selector("title", :content => "Sign in")
+      end
+      it " should re- render to new page" do
+        post :create , :session => @attr
+        response.should render_template('new')
+      end
+      
+      it " should have flash.now message" do
+        post :create , :session => @attr
+        flash.now[:error].should =~ /invalid/i
+      end
+    end # end of failure
+    
+    describe "with valid email and password" do
 
-end
+          before(:each) do
+                  @user = Factory(:user)
+                  @attr = { :email => @user.email, :password => @user.password }
+                end
+
+                it "should sign the user in" do
+                  post :create, :session => @attr
+                  controller.current_user.should == @user
+                  controller.should be_signed_in
+                end
+
+                it "should redirect to the user show page" do
+                  post :create, :session => @attr
+                  response.should redirect_to(user_path(@user))
+                end
+              end
+            end
+          end
+
+          
